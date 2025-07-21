32c7944a831d6219ae1841b6fcb0387242c50b55
varshith pilli <varshith6946@gmail.com>
2025-07-21 10:42:18 +0530

    push: 14(postman scripting)

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,10 +1,11 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta, timezone
 from dotenv import load_dotenv
-from . import schemas
+from . import schemas, database, models
 import os
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
@@ -17,6 +18,8 @@ exp_time = int(os.getenv("EXPIRY_MINUTES"))
 def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.now(timezone.utc) + timedelta(minutes=exp_time)
+    print(datetime.now(timezone.utc))
+    print(expire)
     to_encode.update({"exp": expire})
 
     encoded_jwt = jwt.encode(to_encode, key, algorithm=algo)
@@ -39,9 +42,13 @@ def verify_access_token(token: str, creds_exception):
     
     return token_data
     
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
     creds_exception = HTTPException(status_code=401,
                                     detail=f"Could not validate credentials",
                                     headers={"WWW-Authenticate": "Bearer"})
     
-    return verify_access_token(token, creds_exception)
+    token = verify_access_token(token, creds_exception)
+
+    user = db.query(models.User).filter(models.User.id == token.id).first()
+
+    return user
